e6061bc2d3784a159dbe2f8cdc67fd19eb647fc8
mohan-sh <140883090+mohan-sh@users.noreply.github.com>
2025-01-06 10:19:14 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.set('view engine','ejs');
-
-const userRouter = require('./views/user');
-app.use('/user',userRouter);
-app.listen(3000);
